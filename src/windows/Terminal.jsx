cad4cc62d152450861ccdb37a22d57ddf33e799f
MehdiBouchachi <mehdibch.dev@gmail.com>
2025-11-24 06:23:27 +0100

    implement a HOC for managing window states and add the first window - terminal

---
+++ src/windows/Terminal.jsx
@@ -0,0 +1,51 @@
+import { WindowControls } from "@components";
+import { techStack } from "@constants";
+import WindowWrapper from "@hoc/WindowWrapper";
+import { Check, Flag } from "lucide-react";
+
+function Terminal() {
+  return (
+    <>
+      <div id="window-header">
+        <WindowControls target="terminal" />
+        <h2>Tech Stack</h2>
+      </div>
+      <div className="techstack">
+        <p>
+          <span className="font-bold">@Mehdi % show tech stack</span>
+        </p>
+
+        <div className="label">
+          <p className="w-32">Category</p>
+          <p>Technology</p>
+        </div>
+
+        <ul className="content">
+          {techStack.map(({ category, items }) => (
+            <li className="flex">
+              <Check className="check" size={20} />
+              <h3>{category}</h3>
+              <ul>
+                {items.map((item, i) => (
+                  <li key={i}>
+                    {item} {i < item.length - 1 ? "," : ""}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+        <div className="footnote">
+          <p>
+            <Check size={20} /> 5 of 5 stacks loaded successfully.
+          </p>
+          <p className="text-black">
+            <Flag size={15} fill="black" /> Render time: 23ms
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+const TerminalWindow = WindowWrapper(Terminal, "terminal");
+export default TerminalWindow;
